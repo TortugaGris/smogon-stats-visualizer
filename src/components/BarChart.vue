22d0c5085497f5744a10e82c70d8552f5d58d8e1
Javier Zavala <javierzavalatorres@gmail.com>
2022-07-14 17:17:45 -0400

    feat: add BarChart component

---
+++ src/components/BarChart.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { scaleBand, scaleLinear, max } from "d3";
+const props = defineProps({
+  data: {
+    type: Object,
+    required: true
+  },
+});
+
+const keysSorted = Object.keys(props.data).sort(function(a,b) {
+    return props.data[b]-props.data[a]
+})
+
+const width=450;
+const height= keysSorted.length*24;
+
+const yScale = scaleBand()
+    .domain(keysSorted)
+    .range([0, 2*height]);
+
+const xScale = scaleLinear()
+    .domain([0, max(keysSorted, d => props.data[d])])
+    .range([0, width]);
+</script>
+
+<template>
+    <svg class="center" :width="width" :height="height">
+        <g v-for="(value, key) in data" :transform="'translate(0,'+ (yScale(key)+12) + ')'">
+            <text y="-2">{{key}}</text>
+            <rect
+                :width="xScale(value)"
+                :height="yScale.bandwidth()/2"/>
+        </g>
+    </svg>
+</template>
+
+<style scoped>
+.center {
+    justify-self: center;
+}
+
+svg text {
+    font-size: 1rem;
+    font-weight: bold;
+}
+</style>
--- src/components/TheStats.vue
+++ src/components/TheStats.vue
@@ -1,6 +1,7 @@
 <script setup>
 import ThePokemonImage from './ThePokemonImage.vue'
 import Speedometer from './Speedometer.vue'
+import BarChart from './BarChart.vue'
 import json from '../data/gen8ou-1825.json'
 const pokemons = json.data
 const pokemon_array = Object.keys(pokemons)
@@ -15,7 +16,10 @@ const current_pokemon = pokemons[pokemon_array[index]]
         <Speedometer :usage="current_pokemon.usage"/>
         <div class="chart">{{current_pokemon['Viability Ceiling']}}</div>
         <div class="chart">{{current_pokemon['Raw count']}}</div>
-        <div class="chart">{{current_pokemon['Moves']}}</div>
+        <div class="chart">
+            <h2>Moves</h2>
+            <BarChart :data="current_pokemon['Moves']"/>
+        </div>
         <div class="chart">{{current_pokemon['Abilities']}}</div>
         <div class="chart">{{current_pokemon['Teammates']}}</div>
         <div class="chart">{{current_pokemon['Spreads']}}</div>
@@ -32,9 +36,19 @@ const current_pokemon = pokemons[pokemon_array[index]]
     grid-template-rows: 1fr 2fr 1fr;
     height: 100vh;
     width: 100vw;
+    max-height: 100%;
+}
+
+.chart-container .chart {
+    overflow: auto;
 }
 
 .chart-container .chart {
     border: 1px solid black;
+    padding: 1rem;
+}
+
+.chart-container .chart h2{
+    margin-bottom: 0.5rem; 
 }
 </style>
