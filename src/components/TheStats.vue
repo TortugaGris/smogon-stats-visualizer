fc228e4cd6e56505f9cb4cfe5c1a113ed8d1e626
Javier Zavala <javierzavalatorres@gmail.com>
2022-07-13 21:32:10 -0400

    feat: read json data

--- src/components/TheStats.vue
+++ src/components/TheStats.vue
@@ -1,88 +1,41 @@
 <script setup>
-request('https://www.smogon.com/stats/2022-05/chaos/gen8ou-0.json')
-  .then(data => console.log(data))
+import json from '../data/gen8ou-1825.json'
+const pokemons = json.data
+const pokemon_array = Object.keys(pokemons)
+const index=47
+const pokemon_name = pokemon_array[index].toLowerCase()
 </script>
 
 <template>
-    <div>
-        <div class="nav-bar"> 
-        </div>
-        <div class="content-container">
-            <div class="filter">
-            </div>
-            <div class="content">
-                <div class="chart">
-                </div>
-                <div class="chart">
-                </div>
-                <div class="chart">
-                </div>
-                <div class="chart">
-                </div>
-                <div class="chart">
-                </div>
-                <div class="chart">
-                </div>
-                <div class="chart">
-                </div>
-                <div class="chart">
-                </div>
-                <div class="chart">
-                </div>
-                <div class="chart">
-                </div>
-                <div class="chart">
-                </div>
-                <div class="chart">
-                </div>
-                <div class="chart">
-                </div>
-                <div class="chart">
-                </div>
-                <div class="chart">
-                </div>
-                <div class="chart">
-                </div>
-            </div>
-        </div>
+    <div class="chart-container">
+        <span class="pokesprite pokemon-gen8 profile" :class="pokemon_name"></span>
+        <div class="chart">{{pokemons[pokemon_array[index]].usage}}</div>
+        <div class="chart">{{pokemons[pokemon_array[index]]['Viability Ceiling']}}</div>
+        <div class="chart">{{pokemons[pokemon_array[index]]['Raw count']}}</div>
+        <div class="chart">{{pokemons[pokemon_array[index]]['Moves']}}</div>
+        <div class="chart">{{pokemons[pokemon_array[index]]['Abilities']}}</div>
+        <div class="chart">{{pokemons[pokemon_array[index]]['Teammates']}}</div>
+        <div class="chart">{{pokemons[pokemon_array[index]]['Spreads']}}</div>
+        <div class="chart">{{pokemons[pokemon_array[index]]['Items']}}</div>
+        <div class="chart">{{Object.keys(pokemons[pokemon_array[index]])}}</div>
     </div>
 </template>
 
 <style>
-:root {
-    --nav-bar-size: 18rem;
+@import '@/assets/pokesprite-pokemon-gen8.css';
+.profile {
+    --pixel-size: 5;
+    justify-self: center;
 }
-.nav-bar {
-    background-color: black;
-    position: fixed;
-    width: var(--nav-bar-size);
-    height: 100vh;
-    z-index: 10;
-} 
-.content-container {
-    background-color: white;
-    position: relative;
-    display: block;
-    height: 100vh;
-    width: calc(100vw - var(--nav-bar-size));
-    left: var(--nav-bar-size);
-}
-
-.content-container .filter {
-    background-color: red;
-    position: relative;
-    height: 3rem;
-}
-
-.content-container .content {
+.chart-container {
     display: grid;
     grid-template-columns: repeat(4, 1fr);
     grid-template-rows: 1fr 2fr 1fr;
-    height: calc(100vh - 3rem);
+    height: 100vh;
+    width: 100vw;
 }
 
-.content-container .content .chart {
+.chart-container .chart {
     border: 1px solid black;
-    background-color: LightSkyBlue;
 }
 </style>
---
+++ src/views/StatsView.vue
@@ -0,0 +1,11 @@
+<script setup>
+//import TheWelcome from '@/components/TheWelcome.vue'
+import TheStats from '@/components/TheStats.vue'
+</script>
+
+<template>
+  <main>
+    <TheStats />
+  </main>
+</template>
+
