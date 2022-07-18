b97645673f87238d68ee77ad88038742860f1e4f
Javier Zavala <javierzavalatorres@gmail.com>
2022-07-18 18:03:25 -0400

    feat: navbar ordered by usage and css for active link

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,16 @@
 import { RouterLink, RouterView } from 'vue-router'
 import { usePokemonStore } from '@/stores/pokemons'
 const pokemon_data = usePokemonStore();
+const pokemonsSorted = pokemon_data.keys.sort(function(a,b) {
+  return pokemon_data.pokemons[b].usage - pokemon_data.pokemons[a].usage
+});
 </script>
 
 <template>
   <header>
     <nav>
       <RouterLink 
-        v-for="pokemon in pokemon_data.keys" 
+        v-for="pokemon in pokemonsSorted" 
         :to="{ params: {pokemon: pokemon}}">
           <span 
             class="pokesprite pokemon-gen8 nav-pokemon" 
@@ -42,6 +45,10 @@ main {
 
 nav .nav-pokemon:hover {
   background-color: gray; 
+}
+
+nav .router-link-active .nav-pokemon{
+  background-color: green;
 }
 
 </style>
